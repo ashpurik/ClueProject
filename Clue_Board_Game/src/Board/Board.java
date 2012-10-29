33ec69f6ed6d502d0550cee5ef2b83ede5a34363
Anastasia Shpurik <ashpurik@mines.edu>
2012-10-28 18:37:27 -0600

    did some refactoring to the code

--- Clue_Board_Game/src/Board/Board.java
+++ Clue_Board_Game/src/Board/Board.java
@@ -13,7 +13,7 @@ import java.util.Set;
 import Board.RoomCell.DoorDirection;
 
 public class Board {
-	
+
 	private ArrayList<BoardCell> cells = new ArrayList<BoardCell>();
 	private Map<Character, String> rooms = null;
 	private Map<BoardCell, LinkedList<Integer>> adjMtx;
@@ -25,13 +25,27 @@ public class Board {
 	private String configTwo = "Config2.txt";
 	private String ourlegend = "legend.txt";
 	private String ourboard = "clueboard.csv";
-	
-	
+
+	public Board() {
+		super();
+		adjMtx = new HashMap<BoardCell, LinkedList<Integer>>();
+		targets = new HashSet<BoardCell>();
+		loadConfigFiles();
+		calcAdjacencies();
+	}
+
+	public RoomCell getRoomCellAt(int row, int column){
+		return (RoomCell) cells.get(calcIndex(row, column));	
+	}
+
+	public BoardCell getCellAt(int start) {
+		return cells.get(start);
+	}
+
 	public Set<BoardCell> getTargets(){
 		return targets;
 	}
-	
-	
+
 	public void setNumRows(int numRows) {
 		this.numRows = numRows;
 	}
@@ -55,42 +69,45 @@ public class Board {
 	public int getNumColumns() {
 		return numColumns;
 	}
-	
+
+	public int calcIndex(int row, int column){
+		return (row*numColumns+column);
+	}
+
 	public boolean doorwayWorks(int index, RoomCell.DoorDirection dd) {
 		RoomCell rc = (RoomCell) cells.get(index);
-		if(rc.isDoorway())
-			if(rc.getDoorDirection() == dd)
-				return true;
+		if(rc.isDoorway() && rc.getDoorDirection() == dd)
+			return true;
 		return false;
 	}
-	
+
 	public void calcAdjacencies(){
 		LinkedList<Integer> local;
+
 		for(int i=0; i<numRows; i++) {
 			for(int j=0; j<numColumns; j++) {
 				local = new LinkedList<Integer>();
-				if(cells.get(calcIndex(i,j)).isRoom() && 
-						!cells.get(calcIndex(i,j)).isDoorway()) {
+				if(cells.get(calcIndex(i,j)).isRoom() && !cells.get(calcIndex(i,j)).isDoorway()) {
 					adjMtx.put(cells.get(calcIndex(i,j)), local);
 					continue;
 				}
-				if(cells.get(calcIndex(i,j)).isRoom() && 
-						cells.get(calcIndex(i,j)).isDoorway()) {
+				//Make sure you exit room correctly
+				if(cells.get(calcIndex(i,j)).isRoom() && cells.get(calcIndex(i,j)).isDoorway()) {
 					RoomCell tempRoom = (RoomCell)cells.get(calcIndex(i,j));
 					switch(tempRoom.getDoorDirection()){
-					
+
 					case UP : local.add(numColumns*(i-1)+j);
-							  adjMtx.put(cells.get(calcIndex(i, j)), local);
-							  continue;
+					adjMtx.put(cells.get(calcIndex(i, j)), local);
+					continue;
 					case DOWN : local.add(numColumns*(i+1)+j);
-					  			adjMtx.put(cells.get(calcIndex(i, j)), local);
-					  			continue;
+					adjMtx.put(cells.get(calcIndex(i, j)), local);
+					continue;
 					case LEFT : local.add(numColumns*(i)+(j-1));
-					  			adjMtx.put(cells.get(calcIndex(i, j)), local);
-					  			continue;
+					adjMtx.put(cells.get(calcIndex(i, j)), local);
+					continue;
 					case RIGHT : local.add(numColumns*(i)+(j+1));
-					  			adjMtx.put(cells.get(calcIndex(i, j)), local);
-					  			continue;
+					adjMtx.put(cells.get(calcIndex(i, j)), local);
+					continue;
 					case NONE:
 					}
 				}
@@ -126,20 +143,18 @@ public class Board {
 			}
 		}
 	}
-	
+
 	public void recursiveTargets(int start, int steps) {
 		int place;
 		BoardCell cell = cells.get(start);
 		visited[start] = true;
-		if(steps == 0){
+		if(steps == 0) {
 			targets.add(cell);
 			visited[start] = false;
 			return;
-		}
-		else{
+		} else {
 			LinkedList<Integer> adjacent = getAdjList(start);
-			for(int i=0; i<adjacent.size(); i++)
-			{
+			for(int i=0; i<adjacent.size(); i++) {
 				place = adjacent.get(i);
 				if(visited[place] == false) {
 					if(cells.get(place).isDoorway())
@@ -151,7 +166,7 @@ public class Board {
 		}
 		visited[start] = false;
 	}
-	
+
 	public void calcTargets(int start, int steps){
 		targets = new HashSet<BoardCell>();
 		recursiveTargets(start, steps);
@@ -161,108 +176,91 @@ public class Board {
 	}
 
 	public void loadConfigFiles(){
-		
+
 		try{
-		//FileReader reader = new FileReader(configOne);
-		FileReader reader = new FileReader(ourlegend);
-		Scanner in = new Scanner(reader);
-		RoomCell r;
-		WalkwayCell w;
-		rooms = new HashMap<Character, String>();
-		String input;
-		int rowCount = 0;
-		int totalCount = 0;
-		String name;
-		while(in.hasNext()){
-			name = in.nextLine();
-			char key = name.charAt(0);
-			name = name.substring(3);
-			rooms.put(key, name);
-		}
-		
-		in.close();
-		//reader = new FileReader(configTwo);
-		reader = new FileReader(ourboard);
-		in = new Scanner (reader);
-		while(in.hasNextLine())
-		{
-			String line = in.nextLine();
-			for(int i=0; i<line.length(); i++) {
-				if(line.charAt(i) == ',')
-					i++;
-				r = new RoomCell();
-				w = new WalkwayCell();
-				int end;
-				if(i >= line.length()-1) {
-					end = i+1;
-					input = line.substring(i, end);
-				}
-				else {
-					if(line.charAt(i+1)!= ',') {
-						end = i+2;
+			//FileReader reader = new FileReader(configOne);
+			FileReader reader = new FileReader(ourlegend);
+			Scanner in = new Scanner(reader);
+			RoomCell r;
+			WalkwayCell w;
+			rooms = new HashMap<Character, String>();
+			String input;
+			int rowCount = 0;
+			int totalCount = 0;
+			String name;
+			while(in.hasNext()){
+				name = in.nextLine();
+				char key = name.charAt(0);
+				name = name.substring(3);
+				rooms.put(key, name);
+			}
+
+			in.close();
+			//reader = new FileReader(configTwo);
+			reader = new FileReader(ourboard);
+			in = new Scanner (reader);
+			while(in.hasNextLine())
+			{
+				String line = in.nextLine();
+				for(int i=0; i<line.length(); i++) {
+					if(line.charAt(i) == ',')
+						i++;
+					r = new RoomCell();
+					w = new WalkwayCell();
+					int end;
+					if(i >= line.length()-1) {
+						end = i+1;
+						input = line.substring(i, end);
+					}
+					else {
+						if(line.charAt(i+1)!= ',') {
+							end = i+2;
+						}
+						else
+							end = i+1;
+						input = line.substring(i, end);
+					}
+					r.setInitial(input.charAt(0));
+					if(input.length() > 1)
+					{
+						if (input.charAt(1) == 'U')
+							r.setDoorDirection(DoorDirection.UP);
+
+						if (input.charAt(1) == 'D')
+							r.setDoorDirection(DoorDirection.DOWN);
+
+						if (input.charAt(1) == 'L')
+							r.setDoorDirection(DoorDirection.LEFT);
+
+						if (input.charAt(1) == 'R')
+							r.setDoorDirection(DoorDirection.RIGHT);	
+					}
+					if (input.charAt(0) == 'W')
+					{
+						cells.add(w);
 					}
 					else
-						end = i+1;
-					input = line.substring(i, end);
+					{
+						r.setInitial(input.charAt(0));
+						cells.add(r);
+					}
+					totalCount++;
+					if(end == i+2)
+						i++;
 				}
-				r.setInitial(input.charAt(0));
-				if(input.length() > 1)
-				{
-					if (input.charAt(1) == 'U')
-						r.setDoorDirection(DoorDirection.UP);
-					
-					if (input.charAt(1) == 'D')
-						r.setDoorDirection(DoorDirection.DOWN);
-					
-					if (input.charAt(1) == 'L')
-						r.setDoorDirection(DoorDirection.LEFT);
-					
-					if (input.charAt(1) == 'R')
-						r.setDoorDirection(DoorDirection.RIGHT);	
-				}
-				if (input.charAt(0) == 'W')
-				{
-					cells.add(w);
-				}
-				else
-				{
-					r.setInitial(input.charAt(0));
-					cells.add(r);
-				}
-				totalCount++;
-				if(end == i+2)
-					i++;
+				rowCount++;
 			}
-			rowCount++;
-		}
-		in.close();
+			in.close();
+
+			numRows = rowCount;
+			numColumns = totalCount / rowCount; 
+			visited = new Boolean [totalCount];
+			for (int i=0; i<totalCount; i++)
+				visited[i] = false;
 			
-		numRows = rowCount;
-		numColumns = totalCount / rowCount; 
-		visited = new Boolean [totalCount];
-		for(int i=0; i<totalCount; i++)
-			visited[i] = false;
-		}catch(FileNotFoundException e){System.out.println(e);}
-	}
-	
-	public Board() {
-		super();
-		adjMtx = new HashMap<BoardCell, LinkedList<Integer>>();
-		targets = new HashSet<BoardCell>();
-		loadConfigFiles();
-		calcAdjacencies();
+		} catch(FileNotFoundException e) {
+			System.out.println(e);
+		}
 	}
 
-	public int calcIndex(int row, int column){
-		return ((row)*(numColumns)+column);
-	}
-	
-	public RoomCell getRoomCellAt(int row, int column){
-		return (RoomCell) cells.get(calcIndex(row, column));	
-	}
-	
-	public BoardCell getCellAt(int start) {
-		return cells.get(start);
-	}
-	
 }
