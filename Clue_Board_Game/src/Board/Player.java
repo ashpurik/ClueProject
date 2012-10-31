3a139ff03107124cee1b221ac2772adcaf508a09
eternalearth888 <mdeslis@gmail.com>
2012-10-30 20:11:45 -0600

    working on tests

--- Clue_Board_Game/src/Board/Player.java
+++ Clue_Board_Game/src/Board/Player.java
@@ -4,6 +4,10 @@ public class Player {
 	//uml based
 	private String name;
 	
+	public String getName() {
+		return name;
+	}
+
 	public Card disproveSuggestions(String person, String room, String weapon){
 		return null;
 	}
--- Clue_Board_Game/src/tests/GameSetupTests.java
+++ Clue_Board_Game/src/tests/GameSetupTests.java
@@ -2,13 +2,43 @@ package tests;
 
 import static org.junit.Assert.*;
 
+import org.junit.BeforeClass;
 import org.junit.Test;
 
-public class GameSetupTests {
+import Board.Board;
+import Board.ComputerPlayer;
+import Board.HumanPlayer;
 
+public class GameSetupTests {
+	@BeforeClass
+	public void setUp(){
+		//declaring ComputerPlayer object
+		ComputerPlayer comp = new ComputerPlayer();
+		//declaring HumanPlayer object
+		HumanPlayer human = new HumanPlayer();
+		//creating new  Board object
+		Board board = new Board();
+	}
+	
+	
+	
+	//making sure that we are loading correct players from file
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testLoadFromFile() {
+		//Colonel Mustard - computer
+		assertEquals("Colonel Mustard", comp.getName());
+		assertEquals("Yellow", comp.getColor());
+		assertEquals(4, board.calcIndex(0,4));
+		
+		//Miss Scarlett - computer
+		assertEquals("Miss Scarlett", comp.getName());
+		assertEquals("Red", comp.getColor());
+		assertEquals(403, board.calcIndex(20,3));
+		
+		//Mrs.Peacock - testing human player
+		assertEquals("Mrs. Peacock", human.getName());
+		assertEquals("Blue", human.getColor());
+		assertEquals(339, board.calcIndex(16,19));
 	}
 
 }
