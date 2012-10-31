36c6c082bdfad7192a7ffea4d3a1a5b471a927da
eternalearth888 <mdeslis@gmail.com>
2012-10-30 19:30:33 -0600

    added changes and methods to ComputerPlayer - have yet to add BoardPiece library

--- Clue_Board_Game/src/Board/ComputerPlayer.java
+++ Clue_Board_Game/src/Board/ComputerPlayer.java
@@ -1,5 +1,21 @@
 package Board;
 
-public class ComputerPlayer {
+import java.util.Set;
 
+public class ComputerPlayer {
+	//uml based
+	private char lastRoomVisited;
+	
+	public void pickLocation(Set<BoardPiece> targets){
+		
+	}
+	
+	public void createSuggestion(){
+		
+	}
+	
+	
+	public void updateSeen(Card seen){
+		
+	}
 }
