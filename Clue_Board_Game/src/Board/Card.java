8fdaeac85487ad7dc43ecf0a2d3353750c7e9bae
eternalearth888 <mdeslis@gmail.com>
2012-10-30 19:24:46 -0600

    added getters and setters + comments

--- Clue_Board_Game/src/Board/Card.java
+++ Clue_Board_Game/src/Board/Card.java
@@ -1,10 +1,31 @@
 package Board;
 
 public class Card {
+	//uml based
 	public enum CardType { ROOM, WEAPON, PERSON };
 	
 	public String name;
-	
 	public CardType cardtype;
+
+	//getters
+	public String getName() {
+		return name;
+	}
+
+	public CardType getCardtype() {
+		return cardtype;
+	}
+	
+	//setters
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setCardtype(CardType cardtype) {
+		this.cardtype = cardtype;
+	}
+
+	
+	
 	
 }
