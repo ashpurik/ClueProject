cf647510ba5f2f1c73e822e8400e0e0bca1067ec
eternalearth888 <mdeslis@gmail.com>
2012-10-30 19:27:48 -0600

    added uml stuff to player class

--- Clue_Board_Game/src/Board/Player.java
+++ Clue_Board_Game/src/Board/Player.java
@@ -1,5 +1,11 @@
 package Board;
 
 public class Player {
-
+	//uml based
+	private String name;
+	
+	public Card disproveSuggestions(String person, String room, String weapon){
+		return null;
+	}
+	
 }
